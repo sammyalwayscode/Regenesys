6d6e0a52803b3c875ab87951047dd737bfefd4d4
sammyalwayscode <olorundasamuel2@gmail.com>
2025-04-19 12:26:14 +0100

    Update

--- Batch 3/Express Js/Online-BookShop/controller/store.controller.js
+++ Batch 3/Express Js/Online-BookShop/controller/store.controller.js
@@ -30,7 +30,7 @@ const createBook = async (req, res) => {
       data: createBook,
     });
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     res.status(400).json({
       message: "Failed to create new book",
       data: error,
@@ -38,4 +38,75 @@ const createBook = async (req, res) => {
   }
 };
 
-module.exports = { createBook };
+const getBook = async (req, res) => {
+  try {
+    const getUser = await userModel.findById(req.params.id).populate("shop");
+    res.status(200).json({
+      message: "Your Books",
+      data: getUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to get book",
+      data: error,
+    });
+  }
+};
+
+const getSingleBook = async (req, res) => {
+  try {
+    const bookData = await storeModel.findById(req.params.id);
+    res.status(200).json({
+      message: "Single book gotten",
+      data: bookData,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to get single book",
+      data: error,
+    });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const getUser = await userModel.findById(req.params.id);
+    const removeBook = await storeModel.findByIdAndDelete(req.params.bookID);
+
+    getUser.shop.pull(removeBook);
+    getUser.save();
+
+    res.status(200).json({
+      message: "Deleted Successfully",
+      data: getUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to delete book",
+      data: error,
+    });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const { title, author } = req.body;
+    const updateData = await storeModel.findByIdAndUpdate(
+      req.params.id,
+      { title, author },
+      { new: true }
+    );
+    res.status(201).json({
+      message: "Updated Successfully",
+      data: updateData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: "Failed to Update book",
+      data: error,
+    });
+  }
+};
+
+module.exports = { createBook, getBook, getSingleBook, deleteBook, updateBook };
--- Batch 3/Express Js/Online-BookShop/router/store.router.js
+++ Batch 3/Express Js/Online-BookShop/router/store.router.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { createBook } = require("../controller/store.controller");
+const {
+  createBook,
+  getBook,
+  getSingleBook,
+  deleteBook,
+  updateBook,
+} = require("../controller/store.controller");
 const { upload } = require("../config/multer");
 const router = express.Router();
 
-router.route("/book/:id").post(upload, createBook);
+router.route("/newbook/:id").post(upload, createBook);
+router.route("/book/:id").get(getBook);
+router.route("/singlebook/:id").get(getSingleBook);
+router.route("/removebook/:id/:bookID").delete(deleteBook);
+router.route("/updatebook/:id/").patch(updateBook);
 
 module.exports = router;
