6754d03e8b6b13f29344f07f24ec10c969791430
Sammy <olorundasamuel2@gmail.com>
2025-07-02 19:03:51 +0100

    multer

--- Batch 3/Express Js/ecommerence/index.js
+++ Batch 3/Express Js/ecommerence/index.js
@@ -3,12 +3,15 @@ const app = express();
 const PORT = 3412;
 require("dotenv").config();
 require("./config/db");
+const productRouter = require("./router/product.router");
 
 app.get("/", (req, res) => {
   res.json({
     message: "server up and running",
   });
 });
+
+app.use("/api/product", productRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
